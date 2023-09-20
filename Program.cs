d08bc5481aac847e230d960574fca629340f440b
7nchz <101666746+7nchz@users.noreply.github.com>
2023-09-20 19:26:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число для вычисления частного двух целых чисел:");
+string s = Console.ReadLine();
+float a = float.Parse(s);
+
+Console.WriteLine("Введите второе число для вычисления частного двух целых чисел:");
+string s2 = Console.ReadLine();
+float b = float.Parse(s2);
+
+float c = a / b;
+Console.WriteLine("Частное данных чисел:");
+Console.WriteLine(c);
+Console.ReadLine();
